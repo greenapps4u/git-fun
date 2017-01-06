d4ce8953349b62c75be56eea4f3f0d9d746c9d0f
greenapps4u <greenapps4u@gmail.com>
2017-01-06 10:10:55 +0100

    Dodanie tła
    Add bacground color

---
+++ 2 Tabletki/2 Tabletki/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  2 Tabletki
+//
+//  Created by Linux on 06.01.2017.
+//  Copyright © 2017 pl.mateuszblaszczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.purple
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

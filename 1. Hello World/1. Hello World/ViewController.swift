b0cc224503a21708c83d987feac1f1ae502e78ed
greenapps4u <greenapps4u@gmail.com>
2017-01-06 09:44:14 +0100

    test
    test

---
+++ 1. Hello World/1. Hello World/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  1. Hello World
+//
+//  Created by Linux on 06.01.2017.
+//  Copyright © 2017 pl.mateuszblaszczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        print ("Hello World or/lub Witaj Świecie")
+        print ("pierwsza apka")
+        print ("It's first basic app")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
